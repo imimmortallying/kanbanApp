b6cad3bf112487950f65ae2608b7416552fe31ee
Alexey Bylina <imimmortallyingwork@yandex.ru>
2024-02-26 13:46:47 +0700

    app.tsx clened

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,31 +2,12 @@
 import "./styles/index.scss";
 import { MainPage } from "pages/MainPage";
 import { Route, Routes } from "react-router-dom";
-import { DndContext } from "@dnd-kit/core";
 import { classNames } from "shared/lib/classNames/classNames";
-import { Suspense, useContext, useEffect, useState } from "react";
-import { Theme, ThemeContext } from "./providers/ThemeProvider/ThemeContext";
+import { Suspense } from "react";
 import { useTheme } from "./providers/ThemeProvider/useTheme";
-import { useDispatch } from "react-redux";
-import { userActions } from "entities/User/model/slice/userSlice";
-import { InitReduxByToken } from "features/AuthByUsername/model/services/InitReduxByToken";
-import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
-import { useAppDispatch } from "./hooks/hooks";
-
-
 
 
 const App = () => {
-
-    const dispatch = useDispatch ();
-    // useEffect( ()=> {
-    //     dispatch(userActions.initAuthData())
-    // }, [dispatch]) // 32.50 это логика проверки того, авторизован ли пользователь, проверка наличия токена. Как это работает?
-    // работает лишь 1 раз!
-
-
-
-
 
     const { theme } = useTheme();
     // вызвать в нужном месте, добавить кнопку для переключения
